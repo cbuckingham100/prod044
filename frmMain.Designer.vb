353e898ef8a0162d031a5f014f462a10aa65b9f6
cbuckingham100 <66245988+cbuckingham100@users.noreply.github.com>
2020-06-01 13:28:57 +0100

    Add files via upload

---
+++ frmMain.Designer.vb
@@ -0,0 +1,286 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtSerial = New System.Windows.Forms.TextBox()
+        Me.txtPart = New System.Windows.Forms.TextBox()
+        Me.btnLoad = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtStart_time = New System.Windows.Forms.TextBox()
+        Me.txtUse_time = New System.Windows.Forms.TextBox()
+        Me.txtPack_time = New System.Windows.Forms.TextBox()
+        Me.txtScrapped = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.txtOrder_time = New System.Windows.Forms.TextBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.lblMessage = New System.Windows.Forms.Label()
+        Me.lblEXEVersion = New System.Windows.Forms.Label()
+        Me.lblLinxLibVersion = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(291, 294)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(70, 33)
+        Me.btnExit.TabIndex = 0
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(27, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(109, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Serial Number"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(27, 55)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(98, 20)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Part Number"
+        '
+        'txtSerial
+        '
+        Me.txtSerial.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtSerial.Location = New System.Drawing.Point(168, 16)
+        Me.txtSerial.Name = "txtSerial"
+        Me.txtSerial.Size = New System.Drawing.Size(193, 26)
+        Me.txtSerial.TabIndex = 3
+        '
+        'txtPart
+        '
+        Me.txtPart.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPart.Location = New System.Drawing.Point(168, 52)
+        Me.txtPart.Name = "txtPart"
+        Me.txtPart.Size = New System.Drawing.Size(193, 26)
+        Me.txtPart.TabIndex = 4
+        '
+        'btnLoad
+        '
+        Me.btnLoad.Location = New System.Drawing.Point(215, 294)
+        Me.btnLoad.Name = "btnLoad"
+        Me.btnLoad.Size = New System.Drawing.Size(70, 33)
+        Me.btnLoad.TabIndex = 5
+        Me.btnLoad.Text = "Load"
+        Me.btnLoad.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(27, 89)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(82, 20)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Start Time"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(27, 122)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(76, 20)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Use Time"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(27, 190)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(78, 20)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Scrapped"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(27, 157)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(82, 20)
+        Me.Label6.TabIndex = 9
+        Me.Label6.Text = "Pack Time"
+        '
+        'txtStart_time
+        '
+        Me.txtStart_time.BackColor = System.Drawing.Color.Gainsboro
+        Me.txtStart_time.Enabled = False
+        Me.txtStart_time.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtStart_time.Location = New System.Drawing.Point(168, 86)
+        Me.txtStart_time.Name = "txtStart_time"
+        Me.txtStart_time.Size = New System.Drawing.Size(193, 26)
+        Me.txtStart_time.TabIndex = 10
+        '
+        'txtUse_time
+        '
+        Me.txtUse_time.BackColor = System.Drawing.Color.Gainsboro
+        Me.txtUse_time.Enabled = False
+        Me.txtUse_time.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtUse_time.Location = New System.Drawing.Point(168, 119)
+        Me.txtUse_time.Name = "txtUse_time"
+        Me.txtUse_time.Size = New System.Drawing.Size(193, 26)
+        Me.txtUse_time.TabIndex = 11
+        '
+        'txtPack_time
+        '
+        Me.txtPack_time.BackColor = System.Drawing.Color.Gainsboro
+        Me.txtPack_time.Enabled = False
+        Me.txtPack_time.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPack_time.Location = New System.Drawing.Point(168, 154)
+        Me.txtPack_time.Name = "txtPack_time"
+        Me.txtPack_time.Size = New System.Drawing.Size(193, 26)
+        Me.txtPack_time.TabIndex = 12
+        '
+        'txtScrapped
+        '
+        Me.txtScrapped.BackColor = System.Drawing.Color.Gainsboro
+        Me.txtScrapped.Enabled = False
+        Me.txtScrapped.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtScrapped.Location = New System.Drawing.Point(168, 187)
+        Me.txtScrapped.Name = "txtScrapped"
+        Me.txtScrapped.Size = New System.Drawing.Size(193, 26)
+        Me.txtScrapped.TabIndex = 13
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(27, 222)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(87, 20)
+        Me.Label7.TabIndex = 14
+        Me.Label7.Text = "Order Time"
+        '
+        'txtOrder_time
+        '
+        Me.txtOrder_time.BackColor = System.Drawing.Color.Gainsboro
+        Me.txtOrder_time.Enabled = False
+        Me.txtOrder_time.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtOrder_time.Location = New System.Drawing.Point(168, 219)
+        Me.txtOrder_time.Name = "txtOrder_time"
+        Me.txtOrder_time.Size = New System.Drawing.Size(193, 26)
+        Me.txtOrder_time.TabIndex = 15
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'lblMessage
+        '
+        Me.lblMessage.AutoSize = True
+        Me.lblMessage.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblMessage.Location = New System.Drawing.Point(27, 259)
+        Me.lblMessage.Name = "lblMessage"
+        Me.lblMessage.Size = New System.Drawing.Size(74, 20)
+        Me.lblMessage.TabIndex = 16
+        Me.lblMessage.Text = "Message"
+        '
+        'lblEXEVersion
+        '
+        Me.lblEXEVersion.AutoSize = True
+        Me.lblEXEVersion.Location = New System.Drawing.Point(12, 314)
+        Me.lblEXEVersion.Name = "lblEXEVersion"
+        Me.lblEXEVersion.Size = New System.Drawing.Size(28, 13)
+        Me.lblEXEVersion.TabIndex = 17
+        Me.lblEXEVersion.Text = "v1.0"
+        '
+        'lblLinxLibVersion
+        '
+        Me.lblLinxLibVersion.AutoSize = True
+        Me.lblLinxLibVersion.Location = New System.Drawing.Point(12, 332)
+        Me.lblLinxLibVersion.Name = "lblLinxLibVersion"
+        Me.lblLinxLibVersion.Size = New System.Drawing.Size(28, 13)
+        Me.lblLinxLibVersion.TabIndex = 18
+        Me.lblLinxLibVersion.Text = "v1.0"
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(378, 353)
+        Me.Controls.Add(Me.lblLinxLibVersion)
+        Me.Controls.Add(Me.lblEXEVersion)
+        Me.Controls.Add(Me.lblMessage)
+        Me.Controls.Add(Me.txtOrder_time)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.txtScrapped)
+        Me.Controls.Add(Me.txtPack_time)
+        Me.Controls.Add(Me.txtUse_time)
+        Me.Controls.Add(Me.txtStart_time)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.btnLoad)
+        Me.Controls.Add(Me.txtPart)
+        Me.Controls.Add(Me.txtSerial)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnExit)
+        Me.MinimizeBox = False
+        Me.Name = "frmMain"
+        Me.Text = "PROD044"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnExit As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtSerial As TextBox
+    Friend WithEvents txtPart As TextBox
+    Friend WithEvents btnLoad As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtStart_time As TextBox
+    Friend WithEvents txtUse_time As TextBox
+    Friend WithEvents txtPack_time As TextBox
+    Friend WithEvents txtScrapped As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents txtOrder_time As TextBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents lblMessage As Label
+    Friend WithEvents lblEXEVersion As Label
+    Friend WithEvents lblLinxLibVersion As Label
+End Class
---
+++ frmMain.vb
@@ -0,0 +1,293 @@
+﻿
+Imports LinxLib
+Imports LinxLib.DataLib
+Imports LinxLib.CommonLib
+
+
+Public Class frmMain
+
+    Private TextBoxOrder As New Dictionary(Of TextBox, TextBox)()
+
+    Public sExeVersion As String = "1.0"
+
+    Dim bTimerFlag As Boolean
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        TextBoxOrder.Add(txtSerial, txtPart)
+        TextBoxOrder.Add(txtPart, txtSerial)
+
+        txtSerial.Tag = 1
+        txtPart.Tag = 2
+        btnLoad.Tag = 3
+
+        AddHandler txtSerial.KeyDown, AddressOf BarcodeInputKeyDown
+        AddHandler txtPart.KeyDown, AddressOf BarcodeInputKeyDown
+
+        '        AddHandler txtSerial.Leave, AddressOf BarcodeInputLeave
+        AddHandler txtPart.Leave, AddressOf btnLoad_Click
+
+        lblMessage.Text = ""
+        lblEXEVersion.Text = "Exe:" & sExeVersion
+        lblLinxLibVersion.Text = "Lib:" & sLinxLibVersion
+
+    End Sub
+
+    Private Sub Form1_Shown(sender As Object, e As EventArgs) Handles Me.Shown
+        txtSerial.Select()
+    End Sub
+
+
+    Private Sub BarcodeInputKeyDown(sender As Object, e As KeyEventArgs)
+        If e.KeyCode = Keys.Enter AndAlso ActiveControl.[GetType]() = GetType(TextBox) Then
+            Dim nextTextBox As TextBox
+            If TextBoxOrder.TryGetValue(DirectCast(ActiveControl, TextBox), nextTextBox) Then
+                e.Handled = True
+                e.SuppressKeyPress = True
+                nextTextBox.Focus()
+            End If
+        End If
+    End Sub
+
+    Private Sub BarcodeInputLeave(sender As Object, e As EventArgs)
+        If sender.[GetType]() = GetType(TextBox) Then
+            Dim textBox As TextBox = DirectCast(sender, TextBox)
+            If textBox.Tag.[GetType]() = GetType(Integer) Then
+                BarcodeScanned(textBox.Text, CInt(textBox.Tag))
+            End If
+        End If
+    End Sub
+
+
+    Private Sub BarcodeScanned(barcode As String, order As Integer)
+
+        lblMessage.Text = Convert.ToString(order.ToString() + ": ") & barcode
+
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+
+        End
+
+    End Sub
+
+    Private Sub btnLoad_Click(sender As Object, e As EventArgs) Handles btnLoad.Click
+
+        Dim xBindingsource As BindingSource = Nothing
+        xBindingsource = PrinterExists(txtSerial.Text)
+
+        Dim sMessage As String = "100"
+
+
+        Try
+
+            If Trim(txtSerial.Text) <> "" Then
+
+                sMessage = "101"
+
+                If xBindingsource.Count = 1 Then
+
+                    sMessage = "102"
+
+                    txtPart.Text = CType(xBindingsource.List(0), DataRowView).Item(0).ToString
+                    sMessage = "103"
+                    txtStart_time.Text = CType(xBindingsource.List(0), DataRowView).Item(1).ToString
+                    sMessage = "104"
+
+                    txtUse_time.Text = CType(xBindingsource.List(0), DataRowView).Item(2).ToString
+                    sMessage = "105"
+                    txtPack_time.Text = CType(xBindingsource.List(0), DataRowView).Item(3).ToString
+                    sMessage = "106"
+                    txtScrapped.Text = CType(xBindingsource.List(0), DataRowView).Item(4).ToString
+                    sMessage = "107"
+
+                    txtOrder_time.Text = CType(xBindingsource.List(0), DataRowView).Item(5).ToString
+
+                    sMessage = "108"
+                    lblMessage.Text = "Already scanned"
+                    txtSerial.BackColor = Color.Orange
+                    sMessage = "109"
+                    Application.DoEvents()
+                    sMessage = "110"
+                    WaitHere(20)
+
+                    sMessage = "111"
+                    txtSerial.BackColor = Color.White
+                    lblMessage.Text = ""
+                    Application.DoEvents()
+
+                Else
+
+                    If Trim(txtSerial.Text) <> "" And Trim(txtPart.Text) <> "" Then
+
+                        sMessage = "200"
+
+                        If InsertPrintheadRecentStarts(txtSerial.Text, txtPart.Text) Then
+                            ' Colour the text box
+
+                            sMessage = "201"
+
+                            lblMessage.Text = "Serial number accepted"
+                            txtSerial.BackColor = Color.GreenYellow
+
+                            sMessage = "202"
+                            Application.DoEvents()
+
+                            WaitHere(20)
+
+                            txtSerial.BackColor = Color.White
+                            lblMessage.Text = ""
+                            sMessage = "203"
+                            Application.DoEvents()
+
+                            xBindingsource = PrinterExists(txtSerial.Text)
+                            sMessage = "204"
+                            txtPart.Text = CType(xBindingsource.List(0), DataRowView).Item(0).ToString
+                            sMessage = "205"
+                            txtUse_time.Text = CType(xBindingsource.List(0), DataRowView).Item(2).ToString
+                            sMessage = "206"
+                            txtPack_time.Text = CType(xBindingsource.List(0), DataRowView).Item(3).ToString
+                            sMessage = "207"
+                            txtScrapped.Text = CType(xBindingsource.List(0), DataRowView).Item(4).ToString
+                            sMessage = "208"
+                            txtOrder_time.Text = CType(xBindingsource.List(0), DataRowView).Item(5).ToString
+                            sMessage = "209"
+
+
+                        End If
+
+                    End If
+
+
+                End If
+
+
+            End If
+
+        Catch
+
+            MsgBox("Unable to accept scanned input : " & txtSerial.Text & vbCrLf & "Error: " & Err.Description & sMessage)
+
+        End Try
+
+        txtSerial.Select()
+        Application.DoEvents()
+
+
+    End Sub
+
+
+    Private Function PrinterExists(ByRef sSerial As String) As BindingSource
+
+        PrinterExists = Nothing
+
+        Dim tDatafetch(0) As DataIn
+        Dim xDataAccess As New DataAccess
+
+        Dim sMessage As String = "300"
+
+
+        Try
+
+            sMessage = "301"
+            xDataAccess.Initialise()
+            sMessage = "302"
+            xDataAccess.SetScheme = "dbo"
+            sMessage = "303"
+            xDataAccess.SetTableName = "PrintheadRecentStarts"
+            sMessage = "304"
+            xDataAccess.SetDBWhere = "serial='" & Trim(sSerial) & "'"
+            sMessage = "305"
+
+            ReDim tDatafetch(12)
+            tDatafetch(0).ColName = "product"
+            tDatafetch(1).ColName = "start_time"
+            tDatafetch(2).ColName = "use_time"
+            tDatafetch(3).ColName = "pack_time"
+            tDatafetch(4).ColName = "scrapped"
+            tDatafetch(5).ColName = "order_time"
+            tDatafetch(6).ColName = "step1"
+            tDatafetch(7).ColName = "step2"
+            tDatafetch(8).ColName = "step3"
+            tDatafetch(9).ColName = "potting_success"
+            tDatafetch(10).ColName = "step4"
+            tDatafetch(11).ColName = "vacuum_scan"
+            tDatafetch(12).ColName = "vacuum_success"
+
+            sMessage = "306"
+
+            Dim oBindingSource As BindingSource
+            sMessage = "307"
+            oBindingSource = xDataAccess.GetRecordsDataByID(tDatafetch)
+            sMessage = "308"
+            PrinterExists = oBindingSource
+
+        Catch ex As Exception
+
+            MsgBox("PrinterExists: " & sSerial & vbCrLf & "Error: " & Err.Description & sMessage)
+
+        End Try
+
+
+    End Function
+
+
+    Public Function InsertPrintheadRecentStarts(ByRef sSerial As String, ByRef sPart As String) As Boolean
+
+        Dim tDataInsert(0) As DataIn
+
+        InsertPrintheadRecentStarts = False
+
+        Try
+
+            Dim xDataAccess As New DataAccess
+            xDataAccess.Initialise()
+            xDataAccess.SetScheme = "dbo"
+            xDataAccess.SetTableName = "PrintheadRecentStarts"
+
+            ReDim Preserve tDataInsert(4)
+            tDataInsert(0) = xDataAccess.SQLVar("product", Trim(sPart))
+            tDataInsert(1) = xDataAccess.SQLVar("serial", Trim(sSerial))
+            tDataInsert(2) = xDataAccess.SQLVar("use_time", Format(Now, "yyyy-MM-dd HH:mm:ss"))
+            tDataInsert(3) = xDataAccess.SQLVar("order_time", Format(Now, "yyyy-MM-dd HH:mm:ss"))
+            tDataInsert(4) = xDataAccess.SQLVar("unique_id", "NA")
+
+            InsertPrintheadRecentStarts = xDataAccess.InsertNewRecordArray(tDataInsert)
+
+            xDataAccess.Dispose()
+
+        Catch
+            MsgBox("Unable to insert data into PrintheadRecentStarts")
+
+
+        End Try
+
+
+    End Function
+
+
+
+
+
+
+    Private Sub WaitHere(ByRef iTenthSeconds As Integer)
+
+        Timer1.Interval = iTenthSeconds * 100
+        Timer1.Enabled = True
+
+        Do While Timer1.Enabled
+            Application.DoEvents()
+        Loop
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        Timer1.Enabled = False
+
+    End Sub
+End Class
