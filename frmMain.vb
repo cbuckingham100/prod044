199255274da225d9113b63a82e7460835bdb48fc
Chris Buckingham <chris.buckingham@freshsystems.co.uk>
2020-07-16 12:53:20 +0100

    Rewrite for AS_Part_No

--- frmMain.vb
+++ frmMain.vb
@@ -8,7 +8,7 @@ Public Class frmMain
 
     Private TextBoxOrder As New Dictionary(Of TextBox, TextBox)()
 
-    Public sExeVersion As String = "1.1"
+    Public sExeVersion As String = "1.2"
 
     Dim bTimerFlag As Boolean
 
@@ -18,21 +18,18 @@ Public Class frmMain
         InitializeComponent()
 
         TextBoxOrder.Add(txtSerial, txtPart)
-        TextBoxOrder.Add(txtPart, txtSerial)
 
         txtSerial.Tag = 1
-        txtPart.Tag = 2
-        btnLoad.Tag = 3
+        btnLoad.Tag = 2
 
         AddHandler txtSerial.KeyDown, AddressOf BarcodeInputKeyDown
-        AddHandler txtPart.KeyDown, AddressOf BarcodeInputKeyDown
-
-        '        AddHandler txtSerial.Leave, AddressOf BarcodeInputLeave
-        AddHandler txtPart.Leave, AddressOf btnLoad_Click
+        AddHandler txtSerial.Leave, AddressOf btnLoad_Click
 
         lblMessage.Text = ""
         lblEXEVersion.Text = "Exe:" & sExeVersion
         lblLinxLibVersion.Text = "Lib:" & sLinxLibVersion
+
+        txtSerial.BackColor = Color.Yellow
 
     End Sub
 
@@ -79,84 +76,84 @@ Public Class frmMain
 
         Dim xBindingsource As BindingSource = Nothing
         xBindingsource = PrinterExists(txtSerial.Text)
-
-        Dim sMessage As String = "100"
-
+        Dim rBindingsource As BindingSource = Nothing
 
         Try
 
             If Trim(txtSerial.Text) <> "" Then
 
-                sMessage = "101"
-
                 If xBindingsource.Count = 1 Then
 
-                    sMessage = "102"
+                    txtScannedSerial.Text = txtSerial.Text
+                    txtSerial.Text = ""
 
                     txtPart.Text = CType(xBindingsource.List(0), DataRowView).Item(0).ToString
-                    sMessage = "103"
                     txtStart_time.Text = CType(xBindingsource.List(0), DataRowView).Item(1).ToString
-                    sMessage = "104"
 
                     txtUse_time.Text = CType(xBindingsource.List(0), DataRowView).Item(2).ToString
-                    sMessage = "105"
                     txtPack_time.Text = CType(xBindingsource.List(0), DataRowView).Item(3).ToString
-                    sMessage = "106"
                     txtScrapped.Text = CType(xBindingsource.List(0), DataRowView).Item(4).ToString
-                    sMessage = "107"
-
                     txtOrder_time.Text = CType(xBindingsource.List(0), DataRowView).Item(5).ToString
 
-                    sMessage = "108"
-                    lblMessage.Text = "Already scanned"
+                    lblMessage.Text = "Printhead already scanned!"
                     txtSerial.BackColor = Color.Orange
-                    sMessage = "109"
                     Application.DoEvents()
-                    sMessage = "110"
-                    WaitHere(20)
+                    WaitHere(50)
 
-                    sMessage = "111"
-                    txtSerial.BackColor = Color.White
+                    txtSerial.BackColor = Color.Yellow
                     lblMessage.Text = ""
                     Application.DoEvents()
 
                 Else
 
-                    If Trim(txtSerial.Text) <> "" And Trim(txtPart.Text) <> "" Then
+                    If Trim(txtSerial.Text) <> "" Then
 
-                        sMessage = "200"
+                        rBindingsource = Find_AS_part_no(txtSerial.Text)
 
-                        If InsertPrintheadRecentStarts(txtSerial.Text, txtPart.Text) Then
+                        If rBindingsource.Count = 1 Then
                             ' Colour the text box
-
-                            sMessage = "201"
-
-                            lblMessage.Text = "Serial number accepted"
+                            lblMessage.Text = "New Serial number accepted"
                             txtSerial.BackColor = Color.GreenYellow
 
-                            sMessage = "202"
                             Application.DoEvents()
 
                             WaitHere(20)
 
                             txtSerial.BackColor = Color.White
                             lblMessage.Text = ""
-                            sMessage = "203"
                             Application.DoEvents()
 
-                            xBindingsource = PrinterExists(txtSerial.Text)
-                            sMessage = "204"
-                            txtPart.Text = CType(xBindingsource.List(0), DataRowView).Item(0).ToString
-                            sMessage = "205"
-                            txtUse_time.Text = CType(xBindingsource.List(0), DataRowView).Item(2).ToString
-                            sMessage = "206"
-                            txtPack_time.Text = CType(xBindingsource.List(0), DataRowView).Item(3).ToString
-                            sMessage = "207"
-                            txtScrapped.Text = CType(xBindingsource.List(0), DataRowView).Item(4).ToString
-                            sMessage = "208"
-                            txtOrder_time.Text = CType(xBindingsource.List(0), DataRowView).Item(5).ToString
-                            sMessage = "209"
+                            txtPart.Text = CType(rBindingsource.List(0), DataRowView).Item(2).ToString
 
+                            If Not InsertPrintheadRecentStarts(Trim(txtSerial.Text), Trim(txtPart.Text)) Then
+                                lblMessage.Text = "Not able to insert serial number!"
+                                txtSerial.BackColor = Color.Red
+                                Application.DoEvents()
+                                WaitHere(20)
+                            Else
+                                txtStart_time.Text = Format(Now, "yyyy-MM-dd HH:mm:ss")
+                                txtUse_time.Text = Format(Now, "yyyy-MM-dd HH:mm:ss")
+                                txtPack_time.Text = Format(Now, "yyyy-MM-dd HH:mm:ss")
+                                txtScrapped.Text = ""
+                                txtOrder_time.Text = Format(Now, "yyyy-MM-dd HH:mm:ss")
+                            End If
+
+                            txtScannedSerial.Text = txtSerial.Text
+                            txtSerial.Text = ""
+
+                            txtSerial.BackColor = Color.Yellow
+
+                        Else
+                            lblMessage.Text = "Serial number not found!"
+                            txtSerial.BackColor = Color.Red
+
+                            Application.DoEvents()
+
+                            WaitHere(20)
+
+                            txtSerial.BackColor = Color.Yellow
+                            txtScannedSerial.Text = txtSerial.Text
+                            txtSerial.Text = ""
 
                         End If
 
@@ -168,14 +165,18 @@ Public Class frmMain
 
             End If
 
+            txtSerial.Select()
+
         Catch
+            txtSerial.Select()
+            Application.DoEvents()
+            txtScannedSerial.Text = txtSerial.Text
+            txtSerial.Text = ""
 
-            MsgBox("Unable to accept scanned input : " & txtSerial.Text & vbCrLf & "Error: " & Err.Description & sMessage)
+            txtSerial.BackColor = Color.Yellow
 
+            MsgBox("Unable to accept scanned input : " & txtSerial.Text & vbCrLf & "Error: " & Err.Description)
         End Try
-
-        txtSerial.Select()
-        Application.DoEvents()
 
 
     End Sub
@@ -188,20 +189,11 @@ Public Class frmMain
         Dim tDatafetch(0) As DataIn
         Dim xDataAccess As New DataAccess
 
-        Dim sMessage As String = "300"
-
-
         Try
-
-            sMessage = "301"
             xDataAccess.Initialise()
-            sMessage = "302"
             xDataAccess.SetScheme = "dbo"
-            sMessage = "303"
             xDataAccess.SetTableName = "PrintheadRecentStarts"
-            sMessage = "304"
             xDataAccess.SetDBWhere = "serial='" & Trim(sSerial) & "'"
-            sMessage = "305"
 
             ReDim tDatafetch(12)
             tDatafetch(0).ColName = "product"
@@ -218,17 +210,44 @@ Public Class frmMain
             tDatafetch(11).ColName = "vacuum_scan"
             tDatafetch(12).ColName = "vacuum_success"
 
-            sMessage = "306"
-
             Dim oBindingSource As BindingSource
-            sMessage = "307"
             oBindingSource = xDataAccess.GetRecordsDataByID(tDatafetch)
-            sMessage = "308"
             PrinterExists = oBindingSource
 
         Catch ex As Exception
 
-            MsgBox("PrinterExists: " & sSerial & vbCrLf & "Error: " & Err.Description & sMessage)
+            MsgBox("PrinterExists: " & sSerial & vbCrLf & "Error: " & Err.Description)
+
+        End Try
+
+
+    End Function
+
+    Private Function Find_AS_part_no(ByRef sSerial As String) As BindingSource
+
+        Find_AS_part_no = Nothing
+
+        Dim tDatafetch(0) As DataIn
+        Dim xDataAccess As New DataAccess
+
+        Try
+            xDataAccess.Initialise()
+            xDataAccess.SetScheme = "dbo"
+            xDataAccess.SetTableName = "vw_oracle_ph_details"
+            xDataAccess.SetDBWhere = "serial='" & Trim(sSerial) & "'"
+
+            ReDim tDatafetch(2)
+            tDatafetch(0).ColName = "part_no"
+            tDatafetch(1).ColName = "description"
+            tDatafetch(2).ColName = "spare_printhead"
+
+            Dim oBindingSource As BindingSource
+            oBindingSource = xDataAccess.GetRecordsDataByID(tDatafetch)
+            Find_AS_part_no = oBindingSource
+
+        Catch ex As Exception
+
+            MsgBox("PrinterExists: " & sSerial & vbCrLf & "Error: " & Err.Description)
 
         End Try
 
@@ -249,16 +268,19 @@ Public Class frmMain
             xDataAccess.SetScheme = "dbo"
             xDataAccess.SetTableName = "PrintheadRecentStarts"
 
-            ReDim Preserve tDataInsert(4)
-            tDataInsert(0) = xDataAccess.SQLVar("product", Trim(sPart))
-            tDataInsert(1) = xDataAccess.SQLVar("serial", Trim(sSerial))
-            tDataInsert(2) = xDataAccess.SQLVar("use_time", Format(Now, "yyyy-MM-dd HH:mm:ss"))
+            ReDim Preserve tDataInsert(5)
+            tDataInsert(0) = xDataAccess.SQLVar("product", sPart)
+            tDataInsert(1) = xDataAccess.SQLVar("serial", sSerial)
+            tDataInsert(2) = xDataAccess.SQLVar("start_time", Format(Now, "yyyy-MM-dd HH:mm:ss"))
             tDataInsert(3) = xDataAccess.SQLVar("order_time", Format(Now, "yyyy-MM-dd HH:mm:ss"))
-            tDataInsert(4) = xDataAccess.SQLVar("unique_id", "NA")
+            tDataInsert(4) = xDataAccess.SQLVar("use_time", Format(Now, "yyyy-MM-dd HH:mm:ss"))
+            tDataInsert(5) = xDataAccess.SQLVar("unique_id", "NA")
 
             InsertPrintheadRecentStarts = xDataAccess.InsertNewRecordArray(tDataInsert)
 
             xDataAccess.Dispose()
+
+            InsertPrintheadRecentStarts = True
 
         Catch
             MsgBox("Unable to insert data into PrintheadRecentStarts")
@@ -268,10 +290,6 @@ Public Class frmMain
 
 
     End Function
-
-
-
-
 
 
     Private Sub WaitHere(ByRef iTenthSeconds As Integer)
